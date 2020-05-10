ed64d863e742316db2095abe78655f86aba1277b
sureshkannan <suresh.rajamony@gmail.com>
2020-05-09 21:15:14 -0500

    Changes to webclient
    Changes to webclient

--- mptracehub/src/main/java/com/mp/mptracehub/MPWebviewClient.java
+++ mptracehub/src/main/java/com/mp/mptracehub/MPWebviewClient.java
@@ -5,6 +5,14 @@ import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
 import java.util.Map;
 
 import androidx.annotation.Nullable;
@@ -31,19 +39,43 @@ public class MPWebviewClient extends WebViewClient {
         WebResourceResponse response = super.shouldInterceptRequest(view, request);
         int statusCode=0;
         String responseHeaders="";
+        String httpresponse = "";
+        String mimeType="";
         if(response!=null){
             statusCode = response.getStatusCode();
             Map<String,String> responseHeaderMap = response.getResponseHeaders();
             responseHeaders = responseHeaderMap.toString();
-            String mimeType = response.getMimeType();
+            mimeType = response.getMimeType();
+            httpresponse = getTextFromStream(response.getData());
         }
-        StringBuilder builder = new StringBuilder();
-        builder.append("[URL:");builder.append(url);
-        builder.append(",");
-        builder.append("req.headers:["+requestHeaders+"],");
-        builder.append("res.code:"+statusCode+",");
-        builder.append("res.headers:["+responseHeaders+"],");
-        logCollector.send(builder.toString());
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("typ", "REQ");
+            jsonObject.put("url",url);
+            jsonObject.put("headers",requestHeaders);
+            jsonObject.put("method", method);
+            jsonObject.put("mime", mimeType);
+            jsonObject.put("body", "<Can't resolve body>");
+            logCollector.send(jsonObject.toString());
+            jsonObject.put("typ", "RES");
+            jsonObject.put("url",url);
+            jsonObject.put("headers",responseHeaders);
+            jsonObject.put("method", method);
+            jsonObject.put("mime", mimeType);
+            jsonObject.put("body", httpresponse);
+            logCollector.send(jsonObject.toString());
+        }catch(Exception ex){}
         return response;
+    }
+
+    private String getTextFromStream(InputStream inputStream) {
+        try {
+            BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder data = new StringBuilder();
+            for (String line; (line = r.readLine()) != null; ) {
+                data.append(line).append('\n');
+            }
+            return data.toString();
+        }catch (Exception ex){return "<couldn't read response data>";}
     }
 }
